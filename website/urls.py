54a6caa611b9a54d62f7010a619cf45adb5519e2
Krzysztof <keybi81@gmail.com>
2025-10-30 15:46:45 +0100

    o programie

--- website/urls.py
+++ website/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('gminy/', views.gminy_list, name='gminy_list'),
+    path("", views.home, name="home"),
+    path("gminy/", views.gminy_list, name="gminy_list"),
+    path("o-programie/", views.o_programie, name="o_programie"),
 ]
--- website/views.py
+++ website/views.py
@@ -6,35 +6,36 @@ from .models import Gmina
 
 
 def home(request):
-    return render(request, 'website/base.html')
+    return render(request, "website/base.html")
 
 
 def gminy_list(request):
     """Widok z listą wszystkich gmin z województwami, powiatami i intensywnością pomocy"""
     # Pobierz wszystkie gminy uporządkowane po województwie, powiecie i nazwie
-    gminy = Gmina.objects.all().order_by('wojewodztwo', 'powiat', 'nazwa')
-    
+    gminy = Gmina.objects.all().order_by("wojewodztwo", "powiat", "nazwa")
+
     # Zlicz gminy według województw
     wojewodztwa_stats = {}
     for gmina in gminy:
         if gmina.wojewodztwo not in wojewodztwa_stats:
-            wojewodztwa_stats[gmina.wojewodztwo] = {
-                'count': 0,
-                'intensywnosc': {}
-            }
-        wojewodztwa_stats[gmina.wojewodztwo]['count'] += 1
-        
+            wojewodztwa_stats[gmina.wojewodztwo] = {"count": 0, "intensywnosc": {}}
+        wojewodztwa_stats[gmina.wojewodztwo]["count"] += 1
+
         # Zlicz według intensywności
         intensywnosc = float(gmina.intensywnosc_pomocy)
-        if intensywnosc not in wojewodztwa_stats[gmina.wojewodztwo]['intensywnosc']:
-            wojewodztwa_stats[gmina.wojewodztwo]['intensywnosc'][intensywnosc] = 0
-        wojewodztwa_stats[gmina.wojewodztwo]['intensywnosc'][intensywnosc] += 1
-    
+        if intensywnosc not in wojewodztwa_stats[gmina.wojewodztwo]["intensywnosc"]:
+            wojewodztwa_stats[gmina.wojewodztwo]["intensywnosc"][intensywnosc] = 0
+        wojewodztwa_stats[gmina.wojewodztwo]["intensywnosc"][intensywnosc] += 1
+
     context = {
-        'gminy': gminy,
-        'wojewodztwa_stats': wojewodztwa_stats,
-        'total_count': gminy.count(),
+        "gminy": gminy,
+        "wojewodztwa_stats": wojewodztwa_stats,
+        "total_count": gminy.count(),
     }
-    
-    return render(request, 'website/gminy_list.html', context)
-    return render(request, 'website/gminy_list.html', context)
+
+    return render(request, "website/gminy_list.html", context)
+
+
+def o_programie(request):
+    """Widok z informacjami o programie PSI"""
+    return render(request, "website/o_programie.html")
